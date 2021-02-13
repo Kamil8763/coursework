d002ccf25bfa36853926f60834845a231cd642fb
Камиль Азизов <kamil.azizov.1987@mail.ru>
2021-02-13 21:15:16 +0300

    hf,jnf

---
+++ js/index.js
@@ -0,0 +1,84 @@
+/*код для бургер-меню*/
+window.addEventListener('DOMContentLoaded', function(open) {
+    document.querySelector('.burger').addEventListener('click', function() {
+        document.querySelector('.menu').classList.toggle('is-active')
+    })
+})
+
+/*открытие формы поиск*/
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.form__btn').addEventListener('click', function() {
+        document.querySelector('.form__input').classList.toggle('form-active')
+    })
+})
+
+/*выподающие списки*/
+
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.list1').addEventListener('click', function() {
+        document.querySelector('.submenu').classList.toggle('show')
+    })
+})
+
+
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.list2').addEventListener('click', function() {
+        document.querySelector('.submenu').classList.toggle('show')
+    })
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.list3').addEventListener('click', function() {
+        document.querySelector('.submenu').classList.toggle('show')
+    })
+})
+
+/*
+function list1() {
+  document.getElementById("list1").classList.toggle('show');
+}
+
+function list2() {
+  document.getElementById("list2").classList.toggle('show');
+}
+
+function list3() {
+  document.getElementById("list3").classList.toggle('show');
+}
+
+function list4() {
+  document.getElementById("list4").classList.toggle('show');
+}
+
+function list5() {
+  document.getElementById("list5").classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.click')) {
+
+    var dropdowns = document.querySelector('.submenu');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+/*Swiper для блока Hero*/
+
+const swiper = new Swiper('.hero__swiper', {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 50,
+    height: 100,
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+    },
+});
+
+/**/
